454fe91dc5ea700ed524ea53d39f21744877d19a
natala_semka <seraya.93@inbox.ru>
2023-02-10 22:56:57 +0300

    Добавляет методы-обёртки dayjs

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,39 @@
 import dayjs from 'dayjs'
-// плагин позволяет передавать объект в функцию dayjs()
+// передавать объект в функцию dayjs()
 import objectSupport from 'dayjs/plugin/objectSupport'
 import 'dayjs/locale/ru'
 dayjs.extend(objectSupport)
 dayjs.locale('ru')
+
+/**
+ * @typedef {Object} RawDate
+ * @property {number} year
+ * @property {number} month
+ * @property {number | null} day
+ */
+
+export const dayToday = () => {
+    return dayjs().date()
+}
+
+export const monthToday = () => {
+    return dayjs().month()
+}
+
+export const yearToday = () => {
+    return dayjs().year()
+}
+
+export const fullDateToday = () => {
+    return dayjs().format()
+}
+
+/**
+ * @param {RawDate} date
+ * @returns {string} fullDate
+ */
+export const fullDate = (date) => {
+    return dayjs(date).format()
+}
 
 export { dayjs }
